3f6a80dc3b466c515068fb86d56c397b69579a06
tatiuska <tfweska@gmail.com>
2023-01-19 21:51:42 -0300

    finalização do projeto

--- js/scripts.js
+++ js/scripts.js
@@ -6,29 +6,50 @@ const generatedPasswordElement = document.querySelector("#generated-password");
 // Letras, números e caracteres especiais
 const getLetterLowerCase = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-getLetterLowerCase();
+};
 
 const getLetterUpperCase = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-getLetterUpperCase();
+};
 
 const getNumber = () => {
     return Math.floor(Math.random() * 10).toString();
-}
-
-getNumber();
+};
 
 const getSymbol = () => {
     const symbols = "(){}[]=<>/\|.,!;@#$%&*+-";
     return symbols[Math.floor(Math.random() * symbols.length)];
-}
+};
 
-console.log(getSymbol());
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+    let password = "";
+
+    const passwordLength = 10;
+
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase, 
+        getNumber, 
+        getSymbol
+    ];
+
+    for(i = 0; i < passwordLength; i = i + 4) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+
+            password += randomValue;
+        });
+    };
+
+    password = password.slice(0, passwordLength);
+    
+    
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector("h4").innerText = password;
+};
 
 // Eventos
 // botão que dispara a geração de senha
 generatePasswordButton.addEventListener("click", () => {
-    console.log("teste");
+    generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol);
 });
